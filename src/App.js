5a4036fdc5f3b296ca3a747b379e626f0288b7cd
Dragon <115075047+Dragonbestgg1@users.noreply.github.com>
2024-11-04 10:34:54 +0200

    Fetch function
    API link itself, I think is set up incorrectly that's why it doesn't fetch anything.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const url = "http://api.weatherapi.com/v1/key=1f8a5c56a5744e389e741625240111";
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    return fetch(url)
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
