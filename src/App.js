d9fc9e30bbe4543bf13e86eb7c86e44a89ae80ce
Dragon <115075047+Dragonbestgg1@users.noreply.github.com>
2024-11-04 14:34:34 +0200

    Update App.js
    Added option to change km to miles and also added extra outputs from API

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [data, setData] = useState([]);
+  const [dataFor, setDataFor] = useState([]);
   const [error, setError] = useState(null);
   const [location, setLocation] = useState("");
   const [isFahrenheit, setIsFahrenheit] = useState(false);
+  const [isMPH, setIsMPH] = useState(false);
 
   const url = `https://api.weatherapi.com/v1/current.json?key=1f8a5c56a5744e389e741625240111&q=${location}`;
   const urlFor = `https://api.weatherapi.com/v1/forecast.json?key=1f8a5c56a5744e389e741625240111&q=${location}`;
@@ -35,8 +37,8 @@ function App() {
         }
         return response.json();
       })
-      .then((data) => {
-        setData(data);
+      .then((dataFor) => {
+        setDataFor(dataFor);
         setError(null);
       })
       .catch(handleError);
@@ -49,6 +51,7 @@ function App() {
       return;
     }
     fetchData();
+    fetchDataFor();
   };
 
   const handleError = (error) => {
@@ -86,16 +89,29 @@ function App() {
             onClick={() => setIsFahrenheit(!isFahrenheit)} 
             style={{ cursor: 'pointer' }}
           >
-            Display in {isFahrenheit ? 'Celsius' : 'Fahrenheit'}  {/* Unit conversion trigger */}
+            Display in {isFahrenheit ? 'Celsius' : 'Fahrenheit'}  {/* Unit conversion trigger for degree */}
+          </div>
+        )}
+        {!error && data.current && (
+          <div 
+            onClick={() => setIsMPH(!isMPH)} 
+            style={{ cursor: 'pointer' }}
+          >
+            Display in {isMPH ? 'Kilometers' : 'Miles'}  {/* Unit conversion trigger for speed */}
           </div>
         )}
         {error && <p className="error-message">{error.message}</p>} {/* Display error message to user */}
         {data.current && !error && (
           <div>
             <h2>City: {data.location.name}</h2>
-            <p>Temperature: {isFahrenheit ? data.current.temp_f : data.current.temp_c}°{isFahrenheit ? 'F' : 'C'}</p>  {/* Unit conversion */}
+            <p>Temperature: {isFahrenheit ? data.current.temp_f : data.current.temp_c}°{isFahrenheit ? 'F' : 'C'}</p>
+            <p>Actual feel: {isFahrenheit ? data.current.feelslike_f : data.current.feelslike_c}°{isFahrenheit ? 'F' : 'C'}</p>
+            <p>Wind speed: {isMPH ? data.current.wind_mph + ' miles' : data.current.wind_kph + ' kilometers'} per hour</p>
+            <p>Wind direction: {data.current.wind_dir}</p>
+            <p>Humidity: {data.current.humidity}%</p>
             <p>Condition: {data.current.condition.text}</p>
             <img src={data.current.condition.icon} alt="Weather Icon"></img>
+            <p>UV index: {data.current.uv}</p>
           </div>
         )}
         <p>Powered by WeatherAPI</p>
